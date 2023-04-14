819082941fad6d7223ae99d857e7befff199a000
BluDood <bludood@bludood.com>
2023-04-14 16:15:47 +0200

    Format files

--- endpoints/avatar.js
+++ endpoints/avatar.js
@@ -4,7 +4,7 @@ const sharp = require('sharp')
 require('dotenv').config()
 
 async function fetchImage() {
-  const res = await axios.get(process.env.AVATAR_URL, { responseType: "arraybuffer" })
+  const res = await axios.get(process.env.AVATAR_URL, { responseType: 'arraybuffer' })
   imageSize = (await sharp(res.data).metadata()).width
   imageBuffer = res.data
   lastFetch = Date.now()
@@ -26,9 +26,9 @@ module.exports = {
     if (!process.env.AVATAR_URL) return next()
     const size = parseInt(req.query.size) || null
     // TTL of 1 hour
-    if (!imageBuffer || lastFetch + 60 * 60 * 1000 <= Date.now() ) await fetchImage()
-    if (size > imageSize) return res.send("Size too large")
-    res.contentType(`image/${process.env.AVATAR_URL.split(".")?.pop()?.toLowerCase() || "png"}`)
-    res.send(Buffer.from(await sizeImage(size), "binary"))
+    if (!imageBuffer || lastFetch + 60 * 60 * 1000 <= Date.now()) await fetchImage()
+    if (size > imageSize) return res.send('Size too large')
+    res.contentType(`image/${process.env.AVATAR_URL.split('.')?.pop()?.toLowerCase() || 'png'}`)
+    res.send(Buffer.from(await sizeImage(size), 'binary'))
   }
 }
--- endpoints/contact.js
+++ endpoints/contact.js
@@ -8,19 +8,25 @@ module.exports = {
   description: 'Send me a message.',
   handler: async (req, res, next) => {
     if (!process.env.DISCORD_WEBHOOK) return next()
-    const {name, email, message} = req.body
-    const hook = await axios.post(process.env.DISCORD_WEBHOOK, {
-      embeds: [{
-        title: `${name} <${email}>`,
-        description: message,
-        color: 25855,
-      }],
-      content: `<@${process.env.DISCORD_USER_ID}>`
-    }, {
+    const { name, email, message } = req.body
+    const hook = await axios.post(
+      process.env.DISCORD_WEBHOOK,
+      {
+        embeds: [
+          {
+            title: `${name} <${email}>`,
+            description: message,
+            color: 25855
+          }
+        ],
+        content: `<@${process.env.DISCORD_USER_ID}>`
+      },
+      {
         validateStatus: false
-    })
-    if (hook.status !== 204) return res.status(500).send("An unknown error occorred.")
-    
-    return res.send("Success")
+      }
+    )
+    if (hook.status !== 204) return res.status(500).send('An unknown error occorred.')
+
+    return res.send('Success')
   }
 }
