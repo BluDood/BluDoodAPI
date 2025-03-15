a58bd16dc34c8ce7cb7e4302ef63e77d6d928657
BluDood <bludood@bludood.com>
2025-03-15 11:45:46 +0100

    fix: new logic for getting Spotify token (thanks to KRTirtho of spotube)

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import EventEmitter from 'events'
 import WebSocket from 'ws'
+import { TOTP } from 'totp-generator'
 
 import log from './log.js'
 
@@ -20,7 +21,70 @@ async function subscribe(connection_id: string, token: string) {
   )
 }
 
+function base32FromBytes(bytes: Uint8Array, secretSauce: string): string {
+  let t = 0
+  let n = 0
+  let r = ''
+
+  for (let i = 0; i < bytes.length; i++) {
+    n = (n << 8) | bytes[i]
+    t += 8
+    while (t >= 5) {
+      r += secretSauce[(n >>> (t - 5)) & 31]
+      t -= 5
+    }
+  }
+
+  if (t > 0) {
+    r += secretSauce[(n << (5 - t)) & 31]
+  }
+
+  return r
+}
+
+function cleanBuffer(e: string): Uint8Array {
+  e = e.replace(' ', '')
+  const buffer = new Uint8Array(e.length / 2)
+  for (let i = 0; i < e.length; i += 2) {
+    buffer[i / 2] = parseInt(e.substring(i, i + 2), 16)
+  }
+  return buffer
+}
+
+async function generateTotp(): Promise<{
+  otp: string
+  timestamp: number
+}> {
+  const secretSauce = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
+  const secretCipherBytes = [
+    12, 56, 76, 33, 88, 44, 88, 33, 78, 78, 11, 66, 22, 22, 55, 69, 54
+  ].map((e, t) => e ^ ((t % 33) + 9))
+
+  const secretBytes = cleanBuffer(
+    new TextEncoder()
+      .encode(secretCipherBytes.join(''))
+      .reduce((acc, val) => acc + val.toString(16).padStart(2, '0'), '')
+  )
+
+  const secret = base32FromBytes(secretBytes, secretSauce)
+
+  const res = await axios.get('https://open.spotify.com/server-time')
+  const timestamp = res.data.serverTime * 1000
+
+  const totp = TOTP.generate(secret, {
+    timestamp
+  })
+
+  return {
+    otp: totp.otp,
+    timestamp
+  }
+}
+
 async function getToken(sp_dc: string) {
+  const totp = await generateTotp()
+
   const res = await axios.get(
     'https://open.spotify.com/get_access_token',
     {
@@ -31,7 +95,10 @@ async function getToken(sp_dc: string) {
       },
       params: {
         reason: 'init',
-        productType: 'web-player'
+        productType: 'web-player',
+        totp: totp.otp,
+        totpVer: '5',
+        ts: totp.timestamp
       },
       validateStatus: () => true
     }
